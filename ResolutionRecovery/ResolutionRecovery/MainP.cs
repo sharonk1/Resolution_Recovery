820457aa10486d4daa65755d51321c6a4755d29c
Sharon kotler <sharonkotler1@gmail.com>
2012-11-22 15:44:07 -0800

    these are project files
    make sure you have options.xml file in the same directory as your exe
    file
    watch in folder "exe files"

---
+++ ResolutionRecovery/ResolutionRecovery/MainP.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+//using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+//using System.Xml.Linq;
+
+
+namespace Resolution_Recovery
+{
+    public partial class MainP : Form
+    {
+        public MainP()
+        {
+            InitializeComponent();
+        }
+
+        private void MainP_Load(object sender, EventArgs e)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+
+
+            try
+            {
+                xmlDoc.Load("options.xml");
+                XmlNodeList res = xmlDoc.GetElementsByTagName("resolution1");
+                string resolutionStr = res[0].InnerText;
+                IList<string> resolution = resolutionStr.Split('x');
+
+                string H = resolution[0].ToString();
+                string W = resolution[1].ToString();
+                Resolution.CResolution ChangeRes = new Resolution.CResolution(Convert.ToInt32(H), Convert.ToInt32(W));
+                if (Screen.AllScreens.Length > 1)
+                {
+                    res = xmlDoc.GetElementsByTagName("resolution2");
+                    resolutionStr = res[0].InnerText;
+                    resolution = resolutionStr.Split('x');
+                    H = resolution[0].ToString();
+                    W = resolution[1].ToString();
+                    Resolution.SECResolution ChangeSECRes = new Resolution.SECResolution(Convert.ToInt32(H), Convert.ToInt32(W));
+                }
+            }
+            catch  (Exception msg)
+            {
+                MessageBox.Show("Some error occurred, please try again" + " " + msg.ToString());
+            }
+
+
+            this.Close();
+        }
+
+
+    }
+}
